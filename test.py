b0f10adca5d0bad00490185431ed0da1b9304a32
nrlnbillah <102709093+nrlnbillah@users.noreply.github.com>
2022-04-10 19:10:29 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,103 +1,97 @@
-import matplotlib.pyplot as plt
-import pandas as pd
-import numpy as np
 import streamlit as st
-import sklearn
+import numpy as np
+import pandas as pd
+import plotly_express as px
 
-data=pd.read_csv('vgsales.csv')
-data.isnull().sum()
+### Import data frame
+data_raw = pd.read_csv("ramen-ratings.csv")
 
-#Dropping null values
-data.dropna(subset=['Year', 'Publisher'], inplace=True)
-data.isnull().sum()
+ramen_data_raw = pd.read_csv("ramen-ratings.csv")
+ramen_data_raw.drop(['Review', 'Variety','Top Ten'], axis = 1, inplace = True)
+newcol = ['Brand', 'Style', 'Country', 'Stars']
 
-data.drop(['Name','Rank'], axis=1)
+df = ramen_data_raw.rename(columns = dict(zip(ramen_data_raw.columns, newcol)))
+df['Stars'] = df['Stars'].replace('[%,]', '', regex=True).astype(float)
+pd.options.display.float_format = "{:,.2f}".format
+df['Style'] = df['Style'].apply(lambda x: str(x))
 
-#Encoding str values
-data['Platform']=data['Platform'].astype('category')
-data['Platform_cat']=data['Platform'].cat.codes
-data['Publisher']=data['Publisher'].astype('category')
-data['Publisher_cat']=data['Publisher'].cat.codes
-data['Genre']=data['Genre'].astype('category')
-data['Genre_cat']=data['Genre'].cat.codes
-data.head(6)
 
-#Data split
-X = data.drop(['Global_Sales', 'NA_Sales', 'EU_Sales', 'JP_Sales', 'Other_Sales', 'Platform', 'Genre', 'Publisher','Name','Rank'], axis = 1)
-y = data['Global_Sales'].astype(int) #change from float to int because KNN can't handle y as float
+### --- 'Home' page
 
-from sklearn.model_selection import train_test_split
-Xtrain, Xtest, ytrain, ytest = train_test_split(X, y, random_state = 99)
+col1, col2 = st.columns([2, 1])
 
-## KNN
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import classification_report
+with col1:
+  st.write("""
+  # Ramen Ratings
+  
+  #### This web app analyses **Ramen Ratings** dataset!
+  
+  Disclaimer:  
+  This is a web app using python libraries for learning purpose only.
+  """)
 
-knn = KNeighborsClassifier(n_neighbors = 10)
+with col2:
+  st.write("")
+  st.write("")
+  st.write("")
 
-knn.fit(Xtrain, ytrain)
+st.write("")
+st.write('Before you continue, please read the [terms and conditions](https://www.gnu.org/licenses/gpl-3.0.en.html).')
 
-#print(knn.score(Xtest, ytest))
+show = st.checkbox('I agree the terms and conditions.')
+st.write("")
+st.write("")
 
-y_model = knn.predict(Xtest) 
-accuracy_score(ytest, y_model)
-confusion_matrix(ytest, y_model)
-classification_report(ytest, y_model)
+if show:
 
-#Making dictionaries
-uPlatform=data['Platform'].unique()
-uPublisher=data['Publisher'].unique()
-uGenre=data['Genre'].unique()
-platdict={}
-pubdict={}
-genredict={}
-for i in range(len(uPlatform)):
-    platdict[uPlatform[i]]=i
-for j in range(len(uPublisher)):
-    pubdict[uPublisher[j]]=j
-for k in range(len(uGenre)):
-    genredict[uGenre[k]]=k
 
-#streamlit
+### --- Sidebar layout
 
-st.header("Video Game Sales Analysis and Predictor")
-
-option = st.sidebar.selectbox(
-    'Select a mode',
-     ['About','KNN Results','Model Prediction'])
-
-if option=='About':
-    st.header('About')
-    st.write('Data pulled from vgchartz.com by user GregorySmith on Kaggle on video games with sales exceeding 100,000 copies.')
-    st.write('Data has factors Year Released, Platform, Publisher, and genre, while the output is Global Sales Generated.')
-    st.write('Data was analyzed using KNN model')
-
-elif option=='KNN Results':
+    st.sidebar.write(
+       """Credits to:  
+        - Ts. Dr. Yong Poh Yu  
+        - Dr. Tan Yan Bin"""
+    )
     
-    st.header('KNN Results (n neighbours = 10)')
-    st.write('Accuracy Score')
-    st.write(accuracy_score(ytest, y_model))
-    st.write('Confusion Matrix')
-    st.write(confusion_matrix(ytest, y_model))
-    st.write('Classification Report')
-    st.write(classification_report(ytest, y_model))
+    st.sidebar.write('')
+    
 
-else:
-    st.header('Model Prediction')
-    st.write('Please insert appropriate values for prediction')
-    st.write('Platform')
-    platchoice=platdict[st.selectbox('Platform select',('PS','PS2','PS3','PS4','PSV','XB','PC','XOne','DC','GC','Wii','WiiU'))]
-    st.write('Publisher')
-    pubchoice=pubdict[st.selectbox('Publisher select',('Nintendo','Microsoft Game Studios','Sony Computer Entertainment','Activision'))]
-    st.write('Genre')
-    genchoice=genredict[st.selectbox('Genre select',('Sports', 'Platform', 'Racing', 'Role-Playing', 'Puzzle','Misc','Shooter','Simulation', 'Action', 'Fighting', 'Adventure', 'Strategy'))]
-    st.write('Year Published')
-    yearchoice=st.number_input('Pick a year', 1995, 2021)
-    if st.button('Run!'):
-        x={'Year':[yearchoice],'Platform_cat':[platchoice],'Publisher_cat':[pubchoice],'Genre_cat':[genchoice]}
-        xinput=pd.DataFrame(x)
-        st.write(knn.predict(xinput)) 
-    else:
-        st.write('Have no predictions')
+    ### --- Sidebar selectbox
+    option = st.sidebar.selectbox(
+        'Select your page',
+        ['Dataset', 'Scatterplot', 'Histogram'])
+    
+
+    #### --- Option 'Dataset'
+    if option=='Dataset':
+      st.subheader('Dataset')
+      st.write(
+          """Source:  
+        Kaggle ([Ramen Ratings](https://www.kaggle.com/datasets/residentmario/ramen-ratings))"""
+        )
+      st.write('')
+      show2 = st.checkbox('Display raw data.')
+      if show2:
+        st.write(data_raw)
+        st.write("")      
+      st.write(df)
+      st.write('')
+      st.write(df.describe())
+
+    #### --- Option 'Scatterplot'  
+    if option=='Scatterplot':
+      x_axis = st.sidebar.selectbox(
+          'x-axis',options = list(df.columns))
+      y_axis = st.sidebar.selectbox(
+          'y-axis', options = list(df.columns))
+      scatterplot = px.scatter(data_frame = df, x = x_axis, y = y_axis)
+      st.plotly_chart(scatterplot)
+
+    #### --- Option 'Histogram'    
+    if option=='Histogram':
+      x_axis = st.sidebar.selectbox(
+          'x-axis', options = list(df.columns))
+      y_axis = st.sidebar.selectbox(
+          'y-axis', options = list(df.columns))
+      histo = px.histogram(data_frame = df, x = x_axis, y = y_axis)
+      st.plotly_chart(histo)
